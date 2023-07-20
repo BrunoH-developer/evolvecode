eb4656bf2c92a1a8b1cc19d5f5c3ecbe38e6847b
DEVBrunoHenrique <brunoshenrique1@gmail.com>
2023-07-20 13:22:17 -0300

    .

--- all.js
+++ all.js
@@ -3,3 +3,32 @@ window.scroll({
     behavior:'smooth',
 
 });
+
+const faqItems = document.querySelectorAll('.div-faq');
+
+faqItems.forEach(item => {
+  item.addEventListener('click', () => {
+    // Remove a classe 'active' de todos os itens antes de adicionar ao item clicado
+    faqItems.forEach(item => item.classList.remove('active'));
+
+    // Adiciona a classe 'active' ao item clicado para acionar a transição de escala
+    item.classList.add('active');
+
+    // Obtém o elemento .p-faq dentro do item clicado
+    const faqContent = item.querySelector('.p-faq');
+    
+    // Adiciona/Remove a classe 'show' ao .p-faq para exibir/ocultar suavemente o conteúdo
+    faqContent.classList.toggle('show');
+
+    // Atualiza a exibição dos ícones "add" e "remove" com base no estado do .p-faq
+    const addIcon = item.querySelector('.add');
+    const removeIcon = item.querySelector('.rem');
+    if (faqContent.classList.contains('show')) {
+      addIcon.style.display = 'none';
+      removeIcon.style.display = 'inline-block';
+    } else {
+      addIcon.style.display = 'inline-block';
+      removeIcon.style.display = 'none';
+    }
+  });
+});
